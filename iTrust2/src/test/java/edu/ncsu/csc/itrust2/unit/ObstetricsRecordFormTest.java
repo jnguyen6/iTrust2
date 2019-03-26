17381e8b23d555790700dec0b43f7d5ea2727cbe
jnguyen6 <jimmynguyen@10.137.87.27>
2019-03-26 13:34:51 -0400

    Added and updated unit tests for ObstetricsRecord class and form #24

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/unit/ObstetricsRecordFormTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/unit/ObstetricsRecordFormTest.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package edu.ncsu.csc.itrust2.unit;
 
 import static org.junit.Assert.assertEquals;
@@ -11,7 +8,7 @@ import edu.ncsu.csc.itrust2.forms.hcp.ObstetricsRecordForm;
 import edu.ncsu.csc.itrust2.models.enums.DeliveryMethod;
 
 /**
- * Tests the ObstetricRecordForm Class
+ * Tests the ObstetricsRecordForm Class
  *
  * @author Shukri Qubain (scqubain@ncsu.edu)
  *
@@ -22,25 +19,22 @@ public class ObstetricsRecordFormTest {
      * Tests Constructing the form and setting variables
      */
     @Test
-    public void test () {
+    public void testObstetricsRecordForm () {
         final ObstetricsRecordForm form = new ObstetricsRecordForm();
-        form.setConception( 1999 );
+        form.setConception( 2019 );
         form.setCurrentRecord( false );
         form.setHoursInLabor( 4 );
-        final Long id = (long) 429;
-        final Long lmp = (long) 329;
-        form.setId( id );
+        final String lmp = "2019-03-12";
         form.setLmp( lmp );
         form.setTwins( false );
         form.setWeeksPreg( 36 );
         final DeliveryMethod type = DeliveryMethod.Vaginal;
         form.setType( type );
         final ObstetricsRecordForm test = new ObstetricsRecordForm( form.getType(), form.getLmp(), form.getConception(),
-                form.getWeeksPreg(), form.getHoursInLabor(), form.isTwins(), form.isCurrentRecord(), form.getId() );
+                form.getWeeksPreg(), form.getHoursInLabor(), form.isTwins(), form.isCurrentRecord() );
         assertEquals( form.getConception(), test.getConception() );
         assertEquals( form.isCurrentRecord(), test.isCurrentRecord() );
         assertEquals( form.getHoursInLabor(), test.getHoursInLabor() );
-        assertEquals( form.getId(), test.getId() );
         assertEquals( form.getLmp(), test.getLmp() );
         assertEquals( form.isTwins(), test.isTwins() );
         assertEquals( form.getWeeksPreg(), test.getWeeksPreg() );
