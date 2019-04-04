ae8724e9648c2b1d8706a7861c14c45676a0f6a2
srazdan <srazdan@100.64.13.189>
2019-04-03 20:02:29 -0400

    patientViewingObstetricsRecord feature and step def files for patient veiwing cucumber testing. Contributes to issue #46.

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/PatientViewObstetricsRecordsStepDefs.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/PatientViewObstetricsRecordsStepDefs.java
@@ -5,53 +5,37 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import edu.ncsu.csc.itrust2.forms.admin.ICDCodeForm;
-import edu.ncsu.csc.itrust2.forms.hcp.GeneralCheckupForm;
-import edu.ncsu.csc.itrust2.forms.hcp.OphthalmologySurgeryForm;
-import edu.ncsu.csc.itrust2.models.enums.AppointmentType;
-import edu.ncsu.csc.itrust2.models.enums.EyeSurgeryType;
-import edu.ncsu.csc.itrust2.models.enums.HouseholdSmokingStatus;
-import edu.ncsu.csc.itrust2.models.enums.PatientSmokingStatus;
-import edu.ncsu.csc.itrust2.models.enums.Role;
-import edu.ncsu.csc.itrust2.models.persistent.BasicHealthMetrics;
-import edu.ncsu.csc.itrust2.models.persistent.Diagnosis;
 import edu.ncsu.csc.itrust2.models.persistent.DomainObject;
-import edu.ncsu.csc.itrust2.models.persistent.GeneralCheckup;
-import edu.ncsu.csc.itrust2.models.persistent.GeneralOphthalmology;
-import edu.ncsu.csc.itrust2.models.persistent.Hospital;
-import edu.ncsu.csc.itrust2.models.persistent.ICDCode;
 import edu.ncsu.csc.itrust2.models.persistent.ObstetricsRecord;
-import edu.ncsu.csc.itrust2.models.persistent.OfficeVisit;
-import edu.ncsu.csc.itrust2.models.persistent.OphthalmologySurgery;
-import edu.ncsu.csc.itrust2.models.persistent.Patient;
-import edu.ncsu.csc.itrust2.models.persistent.User;
 
 /**
  * Step definitions for a patient to view their obstetrics records.
- * 
+ *
  * @author Madhura Waghmare (mswaghma)
  */
 public class PatientViewObstetricsRecordsStepDefs extends CucumberTest {
-	
-	private final String baseUrl       = "http://localhost:8080/iTrust2";
-    private final String patientString = "AliceThirteen";
-    private final String obgynString = "tylerOBGYN";
-	
-    
+
+    private final String baseUrl       = "http://localhost:8080/iTrust2";
+    private final String patientString = "JillBob";
+    private final String obgynString   = "tylerOBGYN";
+
+    @Before
+    public void setUp () {
+        DomainObject.deleteAll( ObstetricsRecord.class );
+    }
+
     /**
      * Asserts that the text is on the page
      *
@@ -66,8 +50,8 @@ public class PatientViewObstetricsRecordsStepDefs extends CucumberTest {
             fail();
         }
     }
-    
-    @Given ( "^The patient can have an obstetrics record.$" )
+
+    @Given ( "^There exists a patient in the system who is eligible for an obstetrics record.$" )
     public void pateintExistsInSystem () {
         attemptLogout();
         driver.get( baseUrl );
@@ -92,30 +76,11 @@ public class PatientViewObstetricsRecordsStepDefs extends CucumberTest {
 
         driver.findElement( By.id( "logout" ) ).click();
     }
-    
-    @Given ( "^There exists an obstetrics patient in the system.$" )
-    public void patientExistsObstetricsRecords () {
-        attemptLogout();
 
-        // Create the test User
-        final User user = new User( patientString, "$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.",
-                Role.ROLE_PATIENT, 1 );
-        user.save();
-
-        // The User must also be created as a Patient 
-        // to show up in the list of Patients
-        final Patient patient = new Patient( user.getUsername() );
-        patient.save();
-
-        // All tests can safely assume the existence of the 'hcp', 'admin', and
-        // 'patient' users
-    }
-    
-    
     @Then ( "^I log on as a patient.$" )
     public void loginPatient () {
         attemptLogout();
-        
+
         driver.get( baseUrl );
         final WebElement username = driver.findElement( By.name( "username" ) );
         username.clear();
@@ -128,66 +93,142 @@ public class PatientViewObstetricsRecordsStepDefs extends CucumberTest {
 
         assertEquals( "iTrust2: Patient Home", driver.getTitle() );
     }
-    
+
+    @Then ( "^I log in as an OB/GYN HCP to document an obstetrics record for a patient.$" )
+    public void obgynLogin () {
+        attemptLogout();
+
+        driver.get( baseUrl );
+        final WebElement username = driver.findElement( By.name( "username" ) );
+        username.clear();
+        username.sendKeys( "tylerOBGYN" );
+        final WebElement password = driver.findElement( By.name( "password" ) );
+        password.clear();
+        password.sendKeys( "123456" );
+        final WebElement submit = driver.findElement( By.className( "btn" ) );
+        submit.click();
+        assertTextPresent( "Welcome to iTrust2 - HCP" );
+    }
+
+    @When ( "^I navigate to the document obstetrics record page to document an obstetrics record for a patient.$" )
+    public void navigateToPage () {
+        final WebDriverWait wait = new WebDriverWait( driver, 20 );
+        wait.until( ExpectedConditions.titleContains( "iTrust2: HCP Home" ) );
+        ( (JavascriptExecutor) driver )
+                .executeScript( "document.getElementById('OBGYNHCPDocumentObstetricsRecord').click();" );
+        final WebDriverWait wait2 = new WebDriverWait( driver, 20 );
+        wait2.until( ExpectedConditions.titleContains( "iTrust2: View Patient Obstetrics Records" ) );
+        assertTextPresent( "Current Pregnancy" );
+        assertTextPresent( "Previous Pregnancies" );
+    }
+
+    @And ( "^I select radio button with text (.+) to select a patient.$" )
+    public void selectPatient ( final String username ) {
+        waitForAngular();
+        driver.findElement( By.id( username ) ).click();
+    }
+
+    @And ( "^I enter (.+) for a current obstetrics record for the patient.$" )
+    public void enterLMP ( final String date ) {
+        final WebElement dateElement = driver.findElement( By.name( "currentlmp" ) );
+        dateElement.sendKeys( date.replace( "/", "" ) );
+    }
+
+    @And ( "^I click create obstetrics record to document an obstetrics record for a patient.$" )
+    public void clickToCreateObstetricsRecord () {
+        waitForAngular();
+        driver.findElement( By.name( "submit" ) );
+    }
+
+    @And ( "^I enter (.+), (\\d+), (\\d+), (\\d+), (.+), (.+) for a previous obstetrics record to document an obstetrics record for a patient.$" )
+    public void previousRecordData ( final String date, final int conception, final int weeks, final int hours,
+            final String delivery, final String twins ) {
+        waitForAngular();
+        driver.findElement( By.name( "addPreviousPregnancy" ) ).click();
+        final WebElement dateElement = driver.findElement( By.name( "previouslmp" ) );
+        dateElement.sendKeys( date.replace( "/", "" ) );
+
+        final WebElement conception_year = driver.findElement( By.name( "conception" ) );
+        conception_year.clear();
+        conception_year.sendKeys( Integer.toString( conception ) );
+
+        final WebElement weeks_pregnant = driver.findElement( By.name( "weeksPreg" ) );
+        weeks_pregnant.clear();
+        weeks_pregnant.sendKeys( Integer.toString( weeks ) );
+
+        final WebElement hours_in_labor = driver.findElement( By.name( "hoursInLabor" ) );
+        hours_in_labor.clear();
+        hours_in_labor.sendKeys( Integer.toString( hours ) );
+
+        final Select delivery_dropdown = new Select( driver.findElement( By.id( "type" ) ) );
+        delivery_dropdown.selectByVisibleText( delivery );
+
+        final Select twins_dropdown = new Select( driver.findElement( By.id( "twins" ) ) );
+        twins_dropdown.selectByVisibleText( twins );
+    }
+
+    @And ( "^I click add to document an obstetrics record for a patient.$" )
+    public void addPreviousPregnancy () {
+        driver.findElement( By.name( "add" ) ).click();
+        try {
+            Thread.sleep( 5000 );
+        }
+        catch ( final InterruptedException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @Then ( "^I log out as OB/GYN HCP.$" )
+    public void obgynlogout () {
+        driver.findElement( By.id( "logout" ) ).click();
+        final WebDriverWait wait = new WebDriverWait( driver, 20 );
+        wait.until( ExpectedConditions.titleContains( "iTrust2 :: Login" ) );
+    }
+
     @When ( "^I navigate to the View Obstetrics Records page.$" )
-    public void navigateToView() {
+    public void navigateToView () {
         ( (JavascriptExecutor) driver ).executeScript( "document.getElementById('viewObstetricsRecords').click();" );
 
         assertEquals( "iTrust2: View Obstetrics Records", driver.getTitle() );
-        
+
     }
-    
-    @Then ( "^(.+) is displayed on the page.$")
-    public void noObstetricsRecords ( String text) {
+
+    @Then ( "^(.+) is displayed on the page.$" )
+    public void noObstetricsRecords ( final String text ) {
         waitForAngular();
         assertTextPresent( text );
     }
-    
-    @Then ( "^I log out.$" )
-    public void obgynLogout () {
-        attemptLogout();
-    }
-    
-    @Then( "^I can view the obstetrics record (.+), (.+), (\\d+).$")
-    public void viewEntryPatient( String date, String dueDate, int weeksPreg ) {
+
+    @Then ( "^I can view the obstetrics record (.+), (.+), (\\d+).$" )
+    public void viewEntryPatient ( final String date, final String dueDate, final int weeksPreg ) {
         assertEquals( date, driver.findElement( By.id( "lmp" ) ).getText() );
         assertEquals( dueDate, driver.findElement( By.id( "dueDate" ) ).getText() );
-        assertEquals( weeksPreg, Integer.parseInt(driver.findElement( By.id( "weeksPreg" ) ).getText() ) );
+        assertEquals( weeksPreg, Integer.parseInt( driver.findElement( By.id( "weeksPreg" ) ).getText() ) );
     }
-	
-    @Then( "^I can view the previous obstetrics record (\\d+), (\\d+), (\\d+), (\\d+), (.+), (.+).$")
-    public void viewPreviousEntryPatient( int lmp, int year, int weeksPreg, int labor, String method, String twins ) {
-    	assertEquals( lmp, Integer.parseInt(driver.findElement( By.id( "lmp-0" ) ).getText() ) );
-    	assertEquals( year, Integer.parseInt(driver.findElement( By.id( "conception-0" ) ).getText() ) );
-    	assertEquals( weeksPreg, Integer.parseInt(driver.findElement( By.id( "weeksPreg-0" ) ).getText() ) );
-    	assertEquals( labor, Integer.parseInt(driver.findElement( By.id( "hoursInLabor-0" ) ).getText() ) );
-    	assertEquals( method, driver.findElement( By.id( "type-0" ) ).getText() );
+
+    @Then ( "^I can view the previous obstetrics record (\\d+), (\\d+), (\\d+), (\\d+), (.+), (.+).$" )
+    public void viewPreviousEntryPatient ( final int lmp, final int year, final int weeksPreg, final int labor,
+            final String method, final String twins ) {
+        assertEquals( lmp, Integer.parseInt( driver.findElement( By.id( "lmp-0" ) ).getText() ) );
+        assertEquals( year, Integer.parseInt( driver.findElement( By.id( "conception-0" ) ).getText() ) );
+        assertEquals( weeksPreg, Integer.parseInt( driver.findElement( By.id( "weeksPreg-0" ) ).getText() ) );
+        assertEquals( labor, Integer.parseInt( driver.findElement( By.id( "hoursInLabor-0" ) ).getText() ) );
+        assertEquals( method, driver.findElement( By.id( "type-0" ) ).getText() );
         assertEquals( twins, driver.findElement( By.id( "twins-0" ) ).getText() );
     }
-	
-    @Then( "^I can view both previous and current obstetrics records (\\d+), (.+), (\\d+), (\\d+), (\\d+), (\\d+), (\\d+), (.+).$")
-    public void viewPreviousEntryPatient( int lmp, String dueDate, int weeksPreg, int p_lmp, int year, int weeks, int labor, String method, String twins ) {
-    	assertEquals( lmp, driver.findElement( By.id( "lmp" ) ).getText());
-    	assertEquals( dueDate, Integer.parseInt(driver.findElement( By.id( "dueDate" ) ).getText() ) );
-    	assertEquals( weeksPreg, driver.findElement( By.id( "weeksPreg" ) ).getText());
-    	assertEquals( p_lmp, driver.findElement( By.id( "lmp-0" ) ).getText());
-    	assertEquals( year, Integer.parseInt(driver.findElement( By.id( "conception-0" ) ).getText() ) );
-    	assertEquals( weeks, Integer.parseInt(driver.findElement( By.id( "weeksPreg-0" ) ).getText() ) );
-    	assertEquals( labor, Integer.parseInt(driver.findElement( By.id( "hoursInLabor-0" ) ).getText() ) );
-    	assertEquals( method, driver.findElement( By.id( "type-0" ) ).getText() );
+
+    @Then ( "^I can view both previous and current obstetrics records (\\d+), (.+), (\\d+), (\\d+), (\\d+), (\\d+), (\\d+), (.+).$" )
+    public void viewPreviousEntryPatient ( final int lmp, final String dueDate, final int weeksPreg, final int p_lmp,
+            final int year, final int weeks, final int labor, final String method, final String twins ) {
+        assertEquals( lmp, driver.findElement( By.id( "lmp" ) ).getText() );
+        assertEquals( dueDate, Integer.parseInt( driver.findElement( By.id( "dueDate" ) ).getText() ) );
+        assertEquals( weeksPreg, driver.findElement( By.id( "weeksPreg" ) ).getText() );
+        assertEquals( p_lmp, driver.findElement( By.id( "lmp-0" ) ).getText() );
+        assertEquals( year, Integer.parseInt( driver.findElement( By.id( "conception-0" ) ).getText() ) );
+        assertEquals( weeks, Integer.parseInt( driver.findElement( By.id( "weeksPreg-0" ) ).getText() ) );
+        assertEquals( labor, Integer.parseInt( driver.findElement( By.id( "hoursInLabor-0" ) ).getText() ) );
+        assertEquals( method, driver.findElement( By.id( "type-0" ) ).getText() );
         assertEquals( twins, driver.findElement( By.id( "twins-0" ) ).getText() );
     }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/PatientViewObstetricsRecords.feature
+++ iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/PatientViewObstetricsRecords.feature
@@ -4,11 +4,10 @@ Feature: Patient View Obstetrics Records
   So that I can see the information regarding my pregnancies
   
   Scenario Outline: The patient does not have any existing pregnancies or a current pregnancy
-    Given There exists an obstetrics patient in the system.
+    Given There exists a patient in the system who is eligible for an obstetrics record.
     Then I log on as a patient.
     When I navigate to the View Obstetrics Records page.
     Then <text> is displayed on the page. 
-    Then I log out.
 
     Examples:
     | text          |
@@ -16,57 +15,54 @@ Feature: Patient View Obstetrics Records
     | There are no previous obstetrics records. |
     
   Scenario Outline: View obstetrics record as a Patient who only has a current pregnancy. 
-    Given There exists a patient in the system who can have an obstetrics record.
-    Then I log in as an OB/GYN HCP.
-    When I navigate to the document obstetrics record page.
-    And I select a patient using her <username>.
-    And I enter <LMP> for a current obstetrics record.
-    And I click create obstetrics record.
-    Then I log out.
+    Given There exists a patient in the system who is eligible for an obstetrics record.
+    Then I log in as an OB/GYN HCP to document an obstetrics record for a patient.
+    When I navigate to the document obstetrics record page to document an obstetrics record for a patient.
+    And I select radio button with text <username> to select a patient.
+    And I enter <LMP> for a current obstetrics record for the patient.
+    And I click create obstetrics record to document an obstetrics record for a patient.
+    Then I log out as OB/GYN HCP.
     Then I log on as a patient.
-    And I navigate to the View Obstetrics Records page.
+    When I navigate to the View Obstetrics Records page.
     Then <text> is displayed on the page. 
     Then I can view the obstetrics record <lmp>, <dueDate>, <weeksPreg>.
-    And I am still logged on the iTrust2 homepage. 
 
     Examples:
     | username       | LMP         | text                                       | lmp         | dueDate   | weeksPreg |
-    | AliceThirteen  | 02/14/2017  | There are no previous obstetrics records.  | 02/14/2017  | 09/21/2017| 111       |
+    | JillBob        | 02/14/2017  | There are no previous obstetrics records.  | 02/14/2017  | 09/21/2017| 111       |
    
 
   Scenario Outline: View obstetrics record as a Patient who only has a past pregnancy. 
-    Given There exists a patient in the system who can have an obstetrics record.
-    Then I log in as an OB/GYN HCP.
-    When I navigate to the document obstetrics record page.
-    And I select a patient using her <username>.
-    And I enter <previous_LMP>, <conception_year>, <weeks_pregnant>, <hours_in_labor>, <delivery_method>, <twins> for a previous obstetrics record.
-    And I click add.
-    Then I log out.
+    Given There exists a patient in the system who is eligible for an obstetrics record.
+    Then I log in as an OB/GYN HCP to document an obstetrics record for a patient.
+    When I navigate to the document obstetrics record page to document an obstetrics record for a patient.
+    And I select radio button with text <username> to select a patient.
+    And I enter <previous_LMP>, <conception_year>, <weeks_pregnant>, <hours_in_labor>, <delivery_method>, <twins> for a previous obstetrics record to document an obstetrics record for a patient.
+    And I click add to document an obstetrics record for a patient.
+    Then I log out as OB/GYN HCP.
     Then I log on as a patient.
-    And I navigate to the View Obstetrics Records page.
+    When I navigate to the View Obstetrics Records page.
     Then I can view the previous obstetrics record <previous_LMP>, <conception_year>, <weeks_pregnant>, <hours_in_labor>, <delivery_method>, <twins>.
-    Then text <text> is displayed on the page. 
-    And I am still logged on the iTrust2 homepage. 
+    Then <text> is displayed on the page.  
 
     Examples: 
     | username      | previous_LMP | conception_year | weeks_pregnant| hours_in_labor | delivery_method | twins | text                                      |
     | AliceThirteen | 11/26/2015   | 2015            | 30            | 15             | Cesarean        | false | There are no current obstetrics records.  |
 
   Scenario Outline: View obstetrics record as a Patient who has current and past pregnancies.
-    Given There exists a patient in the system who can have an obstetrics record.
-    Then I log in as an OB/GYN HCP.
-    When I navigate to the document obstetrics record page.
-    And I select a patient using her <username>.
-    And I enter <LMP> for a current obstetrics record.
-    And I click create obstetrics record.
-    And I enter <previous_LMP>, <conception_year>, <weeks_pregnant>, <hours_in_labor>, <delivery_method>, <twins> for a previous obstetrics record.
-    And I click add.
-    Then I log out.
+    Given There exists a patient in the system who is eligible for an obstetrics record.
+    Then I log in as an OB/GYN HCP to document an obstetrics record for a patient.
+    When I navigate to the document obstetrics record page to document an obstetrics record for a patient.
+    And I select radio button with text <username> to select a patient.
+    And I enter <LMP> for a current obstetrics record for the patient.
+    And I click create obstetrics record to document an obstetrics record for a patient.
+    And I enter <previous_LMP>, <conception_year>, <weeks_pregnant>, <hours_in_labor>, <delivery_method>, <twins> for a previous obstetrics record to document an obstetrics record for a patient.
+    And I click add to document an obstetrics record for a patient.
+    Then I log out as OB/GYN HCP.
     Then I log on as a patient.
-    And I navigate to the View Obstetrics Records page.
+    When I navigate to the View Obstetrics Records page.
     Then I can view the obstetrics record <LMP>, <dueDate>, <weeksPreg>.
     Then I can view both previous and current obstetrics records <previous_LMP>, <conception_year>, <weeks_pregnant>, <hours_in_labor>, <delivery_method>, <twins>.
-    And I am still logged on the iTrust2 homepage.
 
     Examples:
     | username       | LMP         | dueDate    | weeksPreg    | previous_LMP | conception_year | weeks_pregnant| hours_in_labor | delivery_method | twins |
